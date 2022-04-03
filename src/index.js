34106243098d1bf747ce412e0f4a046fac675781
yucel.polat <yucel.polat@obss.com>
2022-04-03 22:42:34 +0300

    initial setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "antd/dist/antd.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Main from "./components/pages/main/index";
+import "./assets/css/app.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
